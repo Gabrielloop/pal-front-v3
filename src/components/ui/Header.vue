bd2bc3a07c7d9b2d9c90b4b6666a9d61521838ed
Gabrielloop <gabriel.henin@gmail.com>
2025-06-14 23:09:45 +0200

    Update Header component layout: wrap Logo and title in a flex container and change title text to "Verso"

--- src/components/ui/Header.vue
+++ src/components/ui/Header.vue
@@ -7,8 +7,10 @@ import { createRouter, createWebHistory } from 'vue-router'
   <header class="text-ivory bg-primary shadow">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between">
-        <router-link :to="'/'"><Logo /></router-link>
-        <h1>Titre</h1>
+        <div class="flex items-center gap-4">
+          <router-link :to="'/'"><Logo /></router-link>
+          <h1>Verso</h1>
+        </div>
         <span>recherche</span>
       </div>
     </div>
