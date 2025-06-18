0a32595f5a5ac25e827b1369101eeec4acdfdc64
Gabrielloop <gabriel.henin@gmail.com>
2025-06-18 06:34:47 +0200

    feat: enhance SearchBar UI with improved styling and button integration

--- src/components/ui/SearchBar.vue
+++ src/components/ui/SearchBar.vue
@@ -1,13 +1,21 @@
 <template>
-  <form @submit.prevent="goToSearch">
-    <input v-model="searchTerm" placeholder="Rechercher un livre..." />
-    <button type="submit">🔍</button>
+  <form @submit.prevent="goToSearch" class="flex flex-row items-center gap-2">
+    <input
+      v-model="searchTerm"
+      placeholder="Rechercher un livre..."
+      class="input input-text mb-2 w-full"
+    />
+    <Button :loading="loading" type="submit" variant="valider">
+      <template #icon> <AppIcon name="search" class="mr-2 h-5 w-5" /> </template
+    ></Button>
   </form>
 </template>
-
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import FormContainer from '@/components/ui/FormContainer.vue'
+import Button from '@/components/ui/Button.vue'
+import AppIcon from '@/components/AppIcon.vue'
 
 const searchTerm = ref('')
 const router = useRouter()
@@ -19,16 +27,4 @@ function goToSearch() {
 }
 </script>
 
-<style scoped>
-form {
-  display: flex;
-  gap: 0.5rem;
-}
-input {
-  padding: 0.5rem;
-  flex: 1;
-}
-button {
-  padding: 0.5rem 1rem;
-}
-</style>
+<style scoped></style>
